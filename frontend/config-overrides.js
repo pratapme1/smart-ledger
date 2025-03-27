ca7f80d2dcc92443da8256624a9836571b069e37
pratapme1 <vishnupratapkumar@gmail.com>
2025-03-27 22:42:52 +0530

    removed pwa temp

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,25 +1,7 @@
 module.exports = function override(config, env) {
-    // Add fallbacks for node modules
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      "crypto": require.resolve("crypto-browserify"),
-      "stream": require.resolve("stream-browserify"),
-      "assert": require.resolve("assert"),
-      "zlib": require.resolve("browserify-zlib"),
-      "path": require.resolve("path-browserify"),
-      "buffer": require.resolve("buffer/"),
-      "process": require.resolve("process/browser"),
-      "util": require.resolve("util/"),
-      "os": require.resolve("os-browserify/browser")
-    };
-    
-    // Add process and buffer polyfills
-    const webpack = require('webpack');
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        process: 'process/browser',
-        Buffer: ['buffer', 'Buffer'],
-      }),
+    // Remove the workbox plugin to avoid Node.js polyfill issues
+    config.plugins = config.plugins.filter(plugin => 
+      !plugin.constructor.name.includes('WorkboxWebpackPlugin')
     );
     
     return config;
