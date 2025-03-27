429e874b72fde186ae78994da154935129a13a6f
pratapme1 <vishnupratapkumar@gmail.com>
2025-03-27 22:36:59 +0530

    Add path and other polyfills for Netlify build

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -5,7 +5,22 @@ module.exports = function override(config, env) {
       "crypto": require.resolve("crypto-browserify"),
       "stream": require.resolve("stream-browserify"),
       "assert": require.resolve("assert"),
-      "zlib": require.resolve("browserify-zlib")
+      "zlib": require.resolve("browserify-zlib"),
+      "path": require.resolve("path-browserify"),
+      "buffer": require.resolve("buffer/"),
+      "process": require.resolve("process/browser"),
+      "util": require.resolve("util/"),
+      "os": require.resolve("os-browserify/browser")
     };
+    
+    // Add process and buffer polyfills
+    const webpack = require('webpack');
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer'],
+      }),
+    );
+    
     return config;
   }
